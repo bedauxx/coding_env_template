e75f93bd6fa254e3fd3bfbe55bf73cbc33b4a9eb
小口 征孝 <masakoguchi@crooz-ec-partners.co.jp>
2020-07-06 19:51:35 +0900

    edit

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,16 @@
 //プラグイン
 const gulp = require('gulp')
+const sass = require('gulp-sass')
 const browserSync =require('browser-sync')
 
 //ブラウザシンクさせる対象のフォルダ
-const src = 'store.workman.co.jp/'
+const src = './'
 
 //管理するファイルのパス
 const srcDir = {
-  html:[src + 'item/*.html'],//html第1階層まで
-  css:[src + 'common/css/*.css', src + 'common/css/**/*.css'],//css第2階層まで
+  html:[src + '*.html'],//html第1階層まで
+  css:[src + 'css/'+'*.css', src + './css/**/'+'*.css'],//css第2階層まで
+  scss:[src + 'scss/'+'*.scss'],
 }
 
 //タスク
@@ -24,6 +26,13 @@ gulp.task('css', () => {
   .pipe(browserSync.reload({ stream:true }))
 })
 
+//scss
+gulp.task('scss',() => {
+  gulp.src(srcDir.scss)
+	.pipe(browserSync.reload({ stream:true }))
+})
+
+
 //browser sync
 gulp.task('browser-sync', () => {
   browserSync({
@@ -37,5 +46,5 @@ gulp.task('browser-sync', () => {
 //デフォルト
 gulp.task('default', ['browser-sync'], () => {
 	console.log();
-	gulp.watch([srcDir.html, srcDir.css], ['html', 'css'])
+	gulp.watch([srcDir.html, srcDir.css,srcDir.scss, ], ['html', 'css', 'scss'])
 })
